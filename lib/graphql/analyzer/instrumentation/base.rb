a3be2bac59525f57db7131e9f9d6224eeeba85a5
DerekStride <djgstride@gmail.com>
2018-02-08 15:25:39 -0500

    Add adapter to the payload

--- lib/graphql/analyzer/instrumentation/base.rb
+++ lib/graphql/analyzer/instrumentation/base.rb
@@ -13,17 +13,18 @@ module GraphQL
             if queries.any?
               explain_output = ::ActiveRecord::Base.exec_explain(queries)
               parsed_output = parser.parse(explain_output)
-            end
 
-            # TODO: Merge results when a field makes two types of queries
-            # e.g. path: ['user', 'name'] makes a SQL and ES Query.
-            ctx['graphql-analyzer']['resolvers'] << {
-              'path' => ctx.path,
-              'parentType' => type.name,
-              'fieldName' => field.name,
-              'returnType' => field.type.to_s,
-              'details' => parsed_output
-            }
+              # TODO: Merge results when a field makes two types of queries
+              # e.g. path: ['user', 'name'] makes a SQL and ES Query.
+              ctx['graphql-analyzer']['resolvers'] << {
+                'path' => ctx.path,
+                'adapter' => adapter,
+                'parentType' => type.name,
+                'fieldName' => field.name,
+                'returnType' => field.type.to_s,
+                'details' => parsed_output
+              }
+            end
 
             result
           end
@@ -34,6 +35,10 @@ module GraphQL
 
       def parser
         raise NotImplementedError, "Please override in #{self.class.name}"
+      end
+
+      def adapter
+        @adapter ||= ActiveRecord::Base.connection.instance_values["config"][:adapter]
       end
     end
   end
